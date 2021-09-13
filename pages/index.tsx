2d3971ebb72ef3b6bcb2daf9c5570ad992b7f12c
wiktiav <112339462+wiktiav@users.noreply.github.com>
2021-09-13 16:58:01 +0000

    fixed template

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+
+import Head from "next/head";
+import NavBar from "@components/Header/Header";
+import Copyright from "@components/Copyright/Copyright";
+import Footer from "@components/Footer/Footer";
+import Image from "next/image";
+import SOTD from "@components/SOTD/StockOfTheDay";
+import { Inter } from "@next/font/google";
+import styles from "@styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <SOTD />
+      <Copyright />
+    </>
+  );
+}
