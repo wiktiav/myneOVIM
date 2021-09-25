7ac809d4cffa2d9082a82096f94fd92f990b7ded
wiktiav <112339462+wiktiav@users.noreply.github.com>
2021-09-25 21:00:14 +0000

    Augment documentation.

---
+++ sb-be/models/userinfoModel.tsx
@@ -0,0 +1,19 @@
+
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('logincreds')
+export class userInfo {
+   @PrimaryGeneratedColumn()
+   id! : number;
+
+    @Column()
+    username!: string;
+
+   @Column()
+   email! : string;
+
+   // secret
+   @Column()
+   password! : string;
+
+}
